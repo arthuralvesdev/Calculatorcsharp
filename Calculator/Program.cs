9a52f46c4bc1aa998040f40dd25a56047bb0e9f7
ARTHUR ALVES <contatoarthuralves@gmail.com>
2024-08-21 10:41:54 -0300

    added methods

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,11 +4,50 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Seja bem vindo a Calculadora");
             Console.WriteLine("Escolha a operação: ");
             Console.WriteLine("1 - Adição");
             Console.WriteLine("2 - Subtracao");
             Console.WriteLine("3 - Multiplicacao");
             Console.WriteLine("4 - Divisao");
+            Console.WriteLine("5 - Sair");
+
+        }
+
+        public static void Adicao()
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            var num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            var num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"O resultado da adição é: {num1 + num2}");
+        }
+
+        public static void Subtracao()
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            var num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            var num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"O resultado da subtração é: {num1 - num2}");
+        }
+
+        public static void Multiplicacao()
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            var num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            var num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"O resultado da multiplicação é: {num1 * num2}");
+        }
+
+        public static void Divisao()
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            var num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            var num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"O resultado da divisão é: {num1 / num2}");
         }
     }
 }
