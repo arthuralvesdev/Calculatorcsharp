f47425d0d84fb34a0b0a9af55941476f89662073
ARTHUR ALVES <contatoarthuralves@gmail.com>
2024-08-21 10:06:06 -0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha a operação: ");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtracao");
+            Console.WriteLine("3 - Multiplicacao");
+            Console.WriteLine("4 - Divisao");
+        }
+    }
+}
